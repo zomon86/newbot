1c6336f3bf2ef2da402c4dfd1e855af2f2c93e8c
zomon86 <boydegen00@gmail.com>
2025-01-01 16:33:08 +0700

    newbot

---
+++ README.md
@@ -0,0 +1,33 @@
+>
+
+# DEPLOY IN TERMUX/UBUNTU
+
+```
+pkg upgrade -y && pkg update -y && pkg install nodejs -y && pkg install nodejs-lts -y && pkg install ffmpeg -y && pkg install wget -y && pkg install git -y
+```
+**ATENÇÃO:**
+Será necessário digitar y toda vez que pedir a caixa [y/n]
+---------------------------
+
+**2° Comando**
+```
+termux-setup-storage
+```
+**3° Comando**
+```
+cd /sdcard/Download/404
+```
+**4° Comando**
+```
+npm start
+```
+
+
+
+
+
+//criador: RicZin
+//YouTube: @RicZin 404
+//WhatsApp: +55 11 91933-9968 
+//Comunidade: https://chat.whatsapp.com/I8msWGPLRZ9966WlZsbj81
+//é isso gente espero que goste é nós
---
+++ index.js
@@ -0,0 +1,31 @@
+//criador: RicZin
+//YouTube: @RicZin 404
+//WhatsApp: +55 11 91933-9968 
+//Comunidade: https://chat.whatsapp.com/I8msWGPLRZ9966WlZsbj81
+//é isso gente espero que goste é nós
+
+const {
+   spawn
+} = require('child_process')
+const path = require('path')
+
+function start() {
+   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   console.log([process.argv[0], ...args].join('\n'))
+   let p = spawn(process.argv[0], args, {
+         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+      })
+      .on('message', data => {
+         if (data == 'reset') {
+            console.log('Restarting Bot...')
+            p.kill()
+            start()
+            delete p
+         }
+      })
+      .on('exit', code => {
+         console.error('Exited with code:', code)
+         if (code == '.' || code == 1 || code == 0) start()
+      })
+}
+start()
